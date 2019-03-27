94c0500f72c51cba41a8805b6501a305526d036b
roninTDK <smessiah17@gmail.com>
2019-03-26 22:50:03 -0500

    tried to nest inquire prompts but did not work, going to table this for now

--- liri.js
+++ liri.js
@@ -1,42 +1,64 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+// var bands =
 var inquirer = require("inquirer");
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "liri choices",
-      message: "What can liri search for you?",
-      choices: [
-        "Concert Info",
-        "Search for a Song",
-        "Search for a Movie",
-        "Do What It Says"
-      ]
-    }
-  ])
-  .then(answers => {
-    if (inquirer.prompt[0].choices[0] === true) {
-      console.log("concerts");
+var spotifySearch = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "userInput",
+        message: "What song would you like to search for?"
+      }
+    ])
+    .then(function(search) {
+      // console.log(location.userInput);
 
-      // inquirer
-      // .prompt([
-      //   /* Pass your questions in here */
-      // ])
-      // .then(answers => {
-      //   // Use user feedback for... whatever!!
-      // });
-    } else if (choices[1] === true) {
-      console.log("spotify");
-    } else if (choices[2] === true) {
-      console.log("movie");
-    } else if (choices[3] === true) {
-      console.log("just do it");
-    } else {
-      console.log("make a selection, please....");
-    }
-    console.log("you made a choice");
-  });
+      // Then use the Google Geocoder to Geocode the address
+      keys.spotify(search.userInput, function(err, data) {
+        console.log(JSON.stringify(data, null, 2));
+      });
+    });
+};
+spotifySearch();
+
+////////////tried to nest inquirer prompts but i guess i can't
+// inquirer
+//   .prompt([
+//     {
+//       type: "list",
+//       name: "liri choices",
+//       message: "What can liri search for you?",
+//       choices: ["Concert", "Song", "Movie", "Do What It Says"]
+//     }
+//   ])
+//   .then(answers => {
+//     if (inquirer.Concert === true) {
+//       console.log("concerts");
+
+//       inquirer
+//         .prompt([
+//           {
+//             type: "input",
+//             name: "concert",
+//             message: "What concert info do you want?"
+//           }
+//         ])
+//         .then(answers => {
+//           console.log("concert info here");
+//         });
+//     } //else if (choices[1] === true) {
+//     //   console.log("spotify");
+//     // } else if (choices[2] === true) {
+//     //   console.log("movie");
+//     // } else if (choices[3] === true) {
+//     //   console.log("just do it");
+//     // }
+//     else {
+//       console.log("make a selection, please....");
+//     }
+//     console.log("you made a choice");
+//   });
